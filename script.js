0f00e9b75fd2878dd853c78c0fcc74ee2fa1ffe4
felipe <fl.louzeiro@gmail.com>
2022-05-17 23:41:03 -0300

    feat: implementando envio de menssagem

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function sendMessage(element) {
   let number = prompt("Digite o número:")
   let contentMessage = element.firstElementChild.innerText
-  console.log(contentMessage, number)
+  let link = `https://api.whatsapp.com/send?phone=55${number}&text=${contentMessage}`;
+
+  window.open(link);
 }
 
 module.export = sendMessage;
